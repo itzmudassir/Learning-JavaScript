7fe363196e384581c8dc4cf8515b51feea1e858c
Mudassir <itzmudassir07@gmail.com>
2023-10-21 13:42:14 +0500

    js datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,128 @@
+"use strict";
+
+// 1. Number
+let num = 123;
+console.log(num);
+console.log(typeof num);
+console.log(typeof 123);
+console.log(typeof 123.45);
+console.log(typeof 123.45e6);
+console.log(typeof 123.45e-6);
+console.log(typeof Infinity);
+console.log(typeof -Infinity);  
+console.log(typeof NaN); // Not a Number
+
+// 2. String
+let str = "Hello World";
+console.log(str);
+console.log(typeof str);
+console.log(typeof "Hello World");
+console.log(typeof 'Hello World');
+console.log(typeof `Hello World`);
+console.log(typeof "123");
+console.log(typeof '123');
+console.log(typeof `123`);
+console.log(typeof "123" + "456");
+console.log(typeof "123" + 456);
+
+// 3. Boolean
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+
+// 4. Undefined
+let undef;
+console.log(undef);
+console.log(typeof undef);
+
+// 5. Null
+let nul = null;
+console.log(nul);
+console.log(typeof nul);
+
+// 6. Object
+let obj = {name: "John", age: 30};
+console.log(obj);
+console.log(typeof obj);
+
+// 7. Array
+let arr = [1, 2, 3];
+console.log(arr);
+console.log(typeof arr);
+
+// 8. Function
+let func = function() {};
+console.log(func);
+console.log(typeof func);
+
+// 9. Symbol
+let sym = Symbol();
+console.log(sym);
+console.log(typeof sym);
+
+// 10. BigInt
+let big = 1234567890123456789012345678901234567890n;
+console.log(big);
+console.log(typeof big);
+
+// 11. Date
+let date = new Date();
+console.log(date);
+console.log(typeof date);
+
+// 12. RegExp
+// let regex = /abc/;
+// console.log(regex);
+// console.log(typeof regex);
+
+// 13. Error
+let err = new Error("Something went wrong");
+console.log(err);
+console.log(typeof err);
+
+// 14. Map
+// let map = new Map();
+// console.log(map);
+// console.log(typeof map);
+
+// 15. Set
+// let set = new Set();
+// console.log(set);
+// console.log(typeof set);
+
+// 16. WeakMap
+// let weakMap = new WeakMap();
+// console.log(weakMap);
+// console.log(typeof weakMap);
+
+
+// 17. WeakSet
+// let weakSet = new WeakSet();
+// console.log(weakSet);
+// console.log(typeof weakSet);
+
+// 18. ArrayBuffer
+// let arrayBuffer = new ArrayBuffer();
+// console.log(arrayBuffer);
+// console.log(typeof arrayBuffer);
+
+// 19. SharedArrayBuffer
+// let sharedArrayBuffer = new SharedArrayBuffer();
+// console.log(sharedArrayBuffer);
+// console.log(typeof sharedArrayBuffer);
+
+// 20. DataView
+// let dataView = new DataView();
+// console.log(dataView);
+// console.log(typeof dataView);
+
+// 21. Int8Array
+// let int8Array = new Int8Array();
+// console.log(int8Array);
+// console.log(typeof int8Array);
+
+// 22. Uint8Array
+// let uint8Array = new Uint8Array();
+// console.log(uint8Array);
+// console.log(typeof uint8Array);
+
