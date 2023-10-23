9168f76fe0c1e21449c9ac0be357e6c9f459e02b
Mudassir <itzmudassir07@gmail.com>
2023-10-23 10:42:38 +0500

    Number Conversion

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -10,7 +10,15 @@ score = String(score)
 // Boolean Conversion
 let isLoggedIn = 0      //1 ==> true, 0 ==> false, "" ==> false, "Mudassir" ==> True
 let status = Boolean(isLoggedIn)
-console.log(status);
+// console.log(status);
+
+
+// Number Conversion
+let stringText = "123"
+stringText = Number(stringText)
+console.log(stringText);
+console.log(typeof stringText);
+
 
 
 
