d884904008930e25c9f58d403d20635ea6fa05eb
Mudassir <itzmudassir07@gmail.com>
2023-10-23 10:38:46 +0500

    Boolean Conversion

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -4,7 +4,10 @@ let score = 33
 
 // Changing number to string
 score = String(score)
-console.log(score)
-console.log(typeof score);
+// console.log(score)
+// console.log(typeof score);
 
-
+// Boolean Conversion
+let isLoggedIn = 1
+let status = Boolean(isLoggedIn)
+console.log(status);
