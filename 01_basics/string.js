8ffb1b34c58d9d690059167bfae34e65f6cdc99a
Mudassir <itzmudassir07@gmail.com>
2023-10-25 12:15:04 +0500

    String Practice

---
+++ 01_basics/string.js
@@ -0,0 +1,10 @@
+let name = "mudassir"
+let gpa = 3.86
+
+// console.log(name);
+// console.log(gpa);
+
+// console.log(`My name is ${name} and my gpa is ${gpa}`);
+
+let myName = new String("mudassir") 
+console.log(myName.toUpperCase());
