5c97e7f24f69d848e713b41e26b523ccff08ddf4
Mudassir <itzmudassir07@gmail.com>
2023-10-26 10:58:57 +0500

    Using precision() and LocaleString()

--- 01_basics/numbersAndmath.js
+++ 01_basics/numbersAndmath.js
@@ -5,3 +5,10 @@ let balance = new Number(50)
 
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
+
+
+const estimate = 23.4567
+console.log(estimate.toPrecision(3));
+
+const money = 100000000
+console.log(money.toLocaleString('en-IN'));
