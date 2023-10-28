b064ac134ded05eb88210f9c6a8e14b11cec494e
Mudassir <itzmudassir07@gmail.com>
2023-10-28 11:12:29 +0500

    Dates learning

---
+++ 01_basics/dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toString());
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(myTimestamp.toString());
+// console.log(myTimestamp.toLocaleString());
+
+
+let DateNow = new Date()
+// console.log(DateNow.getMonth()); 
+// console.log(DateNow.getDate()); 
