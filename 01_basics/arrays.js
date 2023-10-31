b665f2591f8311efea77d04abba383879e6a30f4
Mudassir <itzmudassir07@gmail.com>
2023-10-31 13:17:05 +0500

    Arrays practice

---
+++ 01_basics/arrays.js
@@ -0,0 +1,11 @@
+const myarr = [0, 1, 2, 3, 4]
+// console.log(myarr);
+
+// myarr.push(5)
+// myarr.pop()
+const myarr2 = myarr.slice(0,2)
+// console.log(myarr2);
+const myarr3 = myarr.splice(0,2)
+console.log(myarr);
+console.log(myarr3);
+
