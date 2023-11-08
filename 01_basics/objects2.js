da612875ef0af454d1879b6395c1a3f472c67e39
Mudassir <itzmudassir07@gmail.com>
2023-11-08 10:45:42 +0500

    objects learning

---
+++ 01_basics/objects2.js
@@ -0,0 +1,10 @@
+const course = {
+    courseName: 'Arduino',
+    coursePrice: 5000,
+    courseInstructor: 'Mudassir',
+}
+
+const {courseInstructor: instructor} = course;
+
+// console.log(courseInstructor);
+// console.log(instructor);
