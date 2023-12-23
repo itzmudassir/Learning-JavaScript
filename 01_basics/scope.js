2a65490359cdf98339cd345decdcdc8df982ccbd
itzmudassir <itzmudassir07@gmail.com>
2023-12-23 12:17:14 +0500

    Update: Learning Scope in JS

---
+++ 01_basics/scope.js
@@ -0,0 +1,17 @@
+let username = "Mudassir"
+
+function sayHello() {
+    let username = "Mudassir"
+    console.log(`Hello ${username}`)
+}
+
+function welcome(){
+    let name = "Sheikh"
+    console.log(name);
+}
+
+// console.log(name);
+
+// welcome()
+
+// sayHello()
