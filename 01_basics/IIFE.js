4891d7c80b9c4c703d4c9dd3d5dc228d619bbb03
itzmudassir <itzmudassir07@gmail.com>
2023-12-27 11:38:42 +0500

    Update: Learning IIFE in JS

---
+++ 01_basics/IIFE.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions
+
+// Named IIFE
+(function database(){
+    console.log("DB Connected");
+})();
+
+// With arrow function
+
+// Simple IIFE
+(() => {
+    console.log("db connected");
+})()
