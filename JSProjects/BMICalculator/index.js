351b9be47a93fd42a7ce69a8003d92447f1b64e3
itzmudassir <itzmudassir07@gmail.com>
2024-02-13 12:35:28 +0500

    BMI Calculator

---
+++ JSProjects/BMICalculator/index.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#bmiResult");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+    const paragraph = document.createElement('p');
+    if (bmi < 18.5){
+        paragraph.textContent = "Underwight";
+    // Append the paragraph to the results element
+    results.appendChild(paragraph);
+    }
+    else if (bmi >18.5 && bmi < 24.9){
+        paragraph.textContent = "Normal";
+    // Append the paragraph to the results element
+    results.appendChild(paragraph);
+    }
+    else if (bmi >25 && bmi < 29.9){
+        paragraph.textContent = "Overweight";
+    // Append the paragraph to the results element
+    results.appendChild(paragraph);
+    }
+    else if (bmi > 29.9){
+        paragraph.textContent = "Obese";
+    // Append the paragraph to the results element
+    results.appendChild(paragraph);
+    }
+  }
+});
