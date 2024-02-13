55a373714572ef161b5a8b24a120aafd2109e73b
itzmudassir <itzmudassir07@gmail.com>
2024-02-13 11:07:44 +0500

    Color Chager Project

---
+++ JSProjects/colorChanger/index.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function(e){
+        if(e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color = 'black';
+            
+
+        }
+        if(e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color = 'white';
+        }
+        if(e.target.id === 'green') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color = 'white';
+
+        }
+        if(e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color = 'black';
+
+        }
+
+    });
+});
+
