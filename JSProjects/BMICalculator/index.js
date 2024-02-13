8ef247c3089aea198ba69df1d433b5a4cdb0d882
itzmudassir <itzmudassir07@gmail.com>
2024-02-13 12:55:18 +0500

    Added Weight Filters

--- JSProjects/BMICalculator/index.js
+++ JSProjects/BMICalculator/index.js
@@ -12,25 +12,29 @@ form.addEventListener("submit", function (e) {
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+    results.innerHTML = `Your BMI is: ${bmi}`;
     const paragraph = document.createElement('p');
     if (bmi < 18.5){
         paragraph.textContent = "Underwight";
+        paragraph.style.color = 'orange'
     // Append the paragraph to the results element
     results.appendChild(paragraph);
     }
     else if (bmi >18.5 && bmi < 24.9){
         paragraph.textContent = "Normal";
+        paragraph.style.color = 'green'
     // Append the paragraph to the results element
     results.appendChild(paragraph);
     }
     else if (bmi >25 && bmi < 29.9){
         paragraph.textContent = "Overweight";
+        paragraph.style.color = '#FFC300 '
     // Append the paragraph to the results element
     results.appendChild(paragraph);
     }
     else if (bmi > 29.9){
         paragraph.textContent = "Obese";
+        paragraph.style.color = 'red'
     // Append the paragraph to the results element
     results.appendChild(paragraph);
     }
